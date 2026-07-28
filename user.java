965ee6d7ba8683b845d1885cc70df89766c3f065
ssanthiya2708-dot <ssanthiya2708@gmail.com>
2026-07-28 18:05:44 +0530

    Create user.java

---
+++ user.java
@@ -0,0 +1,45 @@
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String fullName;
+    private String email;
+    private String role;
+
+    public User() {}
+
+    public User(int id, String username, String password, String fullName, String email, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.email = email;
+        this.role = role;
+    }
+
+    public User(String username, String password, String fullName, String email, String role) {
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.email = email;
+        this.role = role;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+}
